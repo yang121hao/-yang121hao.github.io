d6684a8ecff2abc58dc16c6b84a05853d197f5c3
yang121hao <a2269198325@163.com>
2020-10-09 13:50:30 +0800

    Site updated: 2020-10-09 13:50:29

---
+++ self/head.js
@@ -0,0 +1,5 @@
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="\assets\js\Meting.min.js"></script>#去掉banner图
+var full_page = document.getElementsByClassName("full_page");
+if (full_page.length != 0) {
+  full_page[0].style.background = "transparent";
+}
