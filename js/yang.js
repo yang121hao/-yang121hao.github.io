7663e6a945b27d746054ad65c0c33b6265cadb06
yang121hao <a2269198325@123.com>
2021-01-12 16:00:43 +0800

    Site updated: 2021-01-12 16:00:34

--- js/yang.js
+++ js/yang.js
@@ -76,7 +76,3 @@ function qipao() {
 
 // 调用气泡方法
 qipao();
-
-$(document).ready(function(e) {
-    $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> Lete乐特');
-});
